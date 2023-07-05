fe207f4d83e8ac7a03554df72f50ab55cfc94e03
artemypopov <artemy.popov.dev@gmail.com>
2023-07-05 23:33:12 +0500

    20 Параметры GET запроса Аннотация @RequestParam

--- src/main/java/ru/popov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/popov/springcourse/controllers/FirstController.java
@@ -1,17 +1,31 @@
 package ru.popov.springcourse.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
-        return "first/hello";
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
+
+         return "first/hello";
     }
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//
+//        System.out.println("Hello, " + name + " " + surname);
+//
+//        return "first/hello";
+//    }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
